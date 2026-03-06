a75f7c519b74cf84c150614ca601ac13d5e43883
Tanmay <tanmayvishe9@gmail.com>
2026-03-06 23:01:51 +0530

    Initial project setup - Prompt2Craft

---
+++ backend/src/main/java/com/prompt2craft/backend/Prompt2craftBackendApplication.java
@@ -0,0 +1,13 @@
+package com.prompt2craft.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prompt2craftBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prompt2craftBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/prompt2craft/backend/Prompt2craftBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prompt2craft.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prompt2craftBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
